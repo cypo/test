d3c39973dedf3e43fcea99812c2632bb60d797e8
cypo <stj123@gmail.com>
2014-10-08 21:01:48 +0200

    ListView from JSON(http)

--- gen/com/example/test/R.java
+++ gen/com/example/test/R.java
@@ -21,21 +21,27 @@ public final class R {
         public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int beer=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int shot=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f090005;
-        public static final int adres1=0x7f090002;
-        public static final int gatunek1=0x7f090001;
-        public static final int nazwa1=0x7f090000;
-        public static final int piwo1=0x7f090003;
-        public static final int shot1=0x7f090004;
+        public static final int action_settings=0x7f090007;
+        public static final int adress=0x7f090002;
+        public static final int beer=0x7f090003;
+        public static final int btngetdata=0x7f090005;
+        public static final int gender=0x7f090001;
+        public static final int listView1=0x7f090006;
+        public static final int name=0x7f090000;
+        public static final int shot=0x7f090004;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_list=0x7f030000;
+        public static final int activity_main=0x7f030001;
     }
     public static final class menu {
-        public static final int main=0x7f080000;
+        public static final int list=0x7f080000;
+        public static final int main=0x7f080001;
     }
     public static final class raw {
         public static final int lodz=0x7f040000;
@@ -44,6 +50,7 @@ public final class R {
         public static final int action_settings=0x7f060001;
         public static final int app_name=0x7f060000;
         public static final int hello_world=0x7f060002;
+        public static final int title_activity_list=0x7f060003;
     }
     public static final class style {
         /** 
--- src/com/example/test/MainActivity.java
+++ src/com/example/test/MainActivity.java
@@ -1,79 +1,134 @@
 package com.example.test;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.view.Menu;
-import java.io.*;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import android.widget.*;
-import android.widget.ArrayAdapter;
 
 
 public class MainActivity extends Activity {
 
-	public TextView nazwa1, gatunek1, adres1, piwo1, shot1;
+	public TextView name, gender, adress, beer, shot;
+	private ListView list;
+	private ListView listView1;
+	public Button btngetdata;
+	ArrayList<HashMap<String, String>> clubList = new ArrayList<HashMap<String, String>>();
 	
+	  //URL to get JSON Array
+	  private static String url = "http://cypo.esy.es/lodz.json";
+
+		//JSON Node Names
+	  private static final String TAG_CLUBS = "kluby";
+	  private static final String TAG_NAME = "name";
+	  private static final String TAG_GENDER = "gender";
+	  private static final String TAG_ADRESS = "adress";
+	  private static final String TAG_BEER = "beer";
+	  private static final String TAG_SHOT = "shot";
+	  
+	  JSONArray android = null;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
-		list();
+		new JSONParse().execute();
+		clubList = new ArrayList<HashMap<String, String>>();
+             
+		btngetdata = (Button)findViewById(R.id.btngetdata);
+	    btngetdata.setOnClickListener(new View.OnClickListener() {
+	      @Override
+	      public void onClick(View view) {
+	             new JSONParse().execute();
+	      }
+           });
 	}
-
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
-	
 		return true;
-
 	}
-    private String readTextFromRawResource(int resourceId) {
-
-        InputStream inputStream = getResources().openRawResource(resourceId);
-        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-
-        int i;
+	
+   class JSONParse extends AsyncTask<String, String, JSONObject> {
+        private ProgressDialog pDialog;
+       @Override
+         protected void onPreExecute() {
+             super.onPreExecute();
+             name = (TextView)findViewById(R.id.name);
+             gender = (TextView)findViewById(R.id.gender);
+             adress = (TextView)findViewById(R.id.adress);
+             beer = (TextView)findViewById(R.id.beer);
+             shot = (TextView)findViewById(R.id.shot);
+              
+             pDialog = new ProgressDialog(MainActivity.this);
+             pDialog.setMessage("Getting Data ...");
+             pDialog.setIndeterminate(false);
+             pDialog.setCancelable(true);
+             pDialog.show();
+       }
+       @Override
+       protected JSONObject doInBackground(String... args) {
+       JSONParser jParser = new JSONParser();
+       // Getting JSON from URL
+       JSONObject json = jParser.getJSONFromUrl(url);
+       return json;
+     }
+      @Override
+        protected void onPostExecute(JSONObject json) {
+        pDialog.dismiss();
         try {
-            i = inputStream.read();
-            while (i != -1) {
-                byteArrayOutputStream.write(i);
-                i = inputStream.read();
-            }
-            inputStream.close();
-        } catch (IOException e) {
-           //Log.e(TAG, "Error while opening resource", e);
-        }
+           // Getting JSON Array from URL
+           android = json.getJSONArray(TAG_CLUBS);
+           for(int i = 0; i < android.length(); i++){
+           JSONObject c = android.getJSONObject(i);
+           // Storing  JSON item in a Variable
+           String name = c.getString(TAG_NAME);
+           String gender = c.getString(TAG_GENDER);
+           String adress = c.getString(TAG_ADRESS);
+           String beer = c.getString(TAG_BEER);
+           String shot = c.getString(TAG_SHOT);
+           // Adding value HashMap key => value
+           HashMap<String, String> map = new HashMap<String, String>();
+           map.put(TAG_NAME, name);
+           map.put(TAG_GENDER, gender);
+           map.put(TAG_ADRESS, adress);
+           map.put(TAG_BEER, beer);
+           map.put(TAG_SHOT, shot);
+           clubList.add(map);
+           
+           list=(ListView)findViewById(R.id.listView1);
+           ListAdapter adapter = new SimpleAdapter(MainActivity.this, clubList,
+               R.layout.activity_list,
+               new String[] { TAG_NAME,TAG_GENDER, TAG_ADRESS, TAG_BEER, TAG_SHOT }, new int[] {
+                   R.id.name,R.id.gender, R.id.adress, R.id.beer, R.id.shot});
+           list.setAdapter(adapter);
+           
+           list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                   @Override
+                   public void onItemClick(AdapterView<?> parent, View view,
+                                           int position, long id) {
+                       Toast.makeText(MainActivity.this, clubList.get(+position).get("name"), Toast.LENGTH_SHORT).show();
+                   }
+               });
+           }
+       } catch (JSONException e) {
+         e.printStackTrace();
+       }
+      }
+   }
+	
+	
 
-        return byteArrayOutputStream.toString();
-    }
-    
-    public void list(){
-		try {
-			JSONObject json = new JSONObject(readTextFromRawResource(R.raw.lodz));
-			//json.getJSONArray("kluby").getJSONObject(0).toString();
-			HomeResponse response = HomeResponse.fromJsonObject(json);
-			nazwa1 = (TextView) findViewById(R.id.nazwa1);
-			gatunek1 = (TextView) findViewById(R.id.gatunek1);
-			adres1 = (TextView) findViewById(R.id.adres1);
-			piwo1 = (TextView) findViewById(R.id.piwo1);
-			shot1 = (TextView) findViewById(R.id.shot1);
-			
-
-			nazwa1.setText(response.kluby[0].toString());
-			gatunek1.setText("Muzyka: "+response.kluby[0].gatunek);
-			adres1.setText("Adres: "+response.kluby[0].adres);
-			piwo1.setText("Piwo: "+Double.toString(response.kluby[0].piwo)+"z³.");
-			shot1.setText("Shot: "+Double.toString(response.kluby[0].shot)+"z³.");
-		
-			
-			} catch (JSONException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-    }
+      
 }
