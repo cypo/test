bb8ce0c4a9065d529c7d14e1a85b527facd89bf1
cyp <cyp@cyp-Komputer>
2014-10-27 19:01:30 +0100

    working json parser, listview

--- src/com/example/test/MainActivity.java
+++ src/com/example/test/MainActivity.java
@@ -1,32 +1,36 @@
 package com.example.test;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import android.os.AsyncTask;
-import android.os.Bundle;
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-
-import android.widget.*;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
 
 	public TextView name, gender, adress, beer, shot;
 	private ListView list;
-	private ListView listView1;
 	public Button btngetdata;
 	ArrayList<HashMap<String, String>> clubList = new ArrayList<HashMap<String, String>>();
-	
+
 	  //URL to get JSON Array
-	  private static String url = "http://cypo.esy.es/lodz.json";
+	  private static final String URL = "http://cypo.esy.es/lodz.json"; //dodaæ wyj¹tek jeœli serwer nie odpowiada/nie istnieje
 
 		//JSON Node Names
 	  private static final String TAG_CLUBS = "kluby";
@@ -36,15 +40,17 @@ public class MainActivity extends Activity {
 	  private static final String TAG_BEER = "beer";
 	  private static final String TAG_SHOT = "shot";
 	  
-	  JSONArray android = null;
+	  private static final String GETTING_DATA = "Pobieranie danych...";
+	  
+	  JSONArray jArray = null;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+				
 		new JSONParse().execute();
-		clubList = new ArrayList<HashMap<String, String>>();
-             
+		             
 		btngetdata = (Button)findViewById(R.id.btngetdata);
 	    btngetdata.setOnClickListener(new View.OnClickListener() {
 	      @Override
@@ -60,28 +66,33 @@ public class MainActivity extends Activity {
 		return true;
 	}
 	
-   class JSONParse extends AsyncTask<String, String, JSONObject> {
-        private ProgressDialog pDialog;
+	//Tworzenie procesu asynctask
+   public class JSONParse extends AsyncTask<String, String, JSONObject> {
+	     private ProgressDialog pDialog;
        @Override
          protected void onPreExecute() {
              super.onPreExecute();
+             //identyfikowanie pól tekstowych w xml
              name = (TextView)findViewById(R.id.name);
              gender = (TextView)findViewById(R.id.gender);
              adress = (TextView)findViewById(R.id.adress);
              beer = (TextView)findViewById(R.id.beer);
              shot = (TextView)findViewById(R.id.shot);
               
+             //definiowanie okna dialogowego ³adowania
              pDialog = new ProgressDialog(MainActivity.this);
-             pDialog.setMessage("Getting Data ...");
+             pDialog.setMessage(GETTING_DATA);
              pDialog.setIndeterminate(false);
              pDialog.setCancelable(true);
              pDialog.show();
        }
+       
+       //zwraca jsona przez klasê JSONParser
        @Override
        protected JSONObject doInBackground(String... args) {
        JSONParser jParser = new JSONParser();
        // Getting JSON from URL
-       JSONObject json = jParser.getJSONFromUrl(url);
+       JSONObject json = jParser.getJSONFromUrl(URL);
        return json;
      }
       @Override
@@ -89,9 +100,9 @@ public class MainActivity extends Activity {
         pDialog.dismiss();
         try {
            // Getting JSON Array from URL
-           android = json.getJSONArray(TAG_CLUBS);
-           for(int i = 0; i < android.length(); i++){
-           JSONObject c = android.getJSONObject(i);
+           jArray = json.getJSONArray(TAG_CLUBS);
+           for(int i=0; i<jArray.length(); i++){
+           JSONObject c = jArray.getJSONObject(i);
            // Storing  JSON item in a Variable
            String name = c.getString(TAG_NAME);
            String gender = c.getString(TAG_GENDER);
@@ -108,16 +119,17 @@ public class MainActivity extends Activity {
            clubList.add(map);
            
            list=(ListView)findViewById(R.id.listView1);
-           ListAdapter adapter = new SimpleAdapter(MainActivity.this, clubList,
-               R.layout.activity_list,
-               new String[] { TAG_NAME,TAG_GENDER, TAG_ADRESS, TAG_BEER, TAG_SHOT }, new int[] {
-                   R.id.name,R.id.gender, R.id.adress, R.id.beer, R.id.shot});
+           ListAdapter adapter = new SimpleAdapter(
+        		   				MainActivity.this, 
+        		   				clubList,
+        		   				R.layout.activity_list,
+        		   				new String[] { TAG_NAME, TAG_GENDER, TAG_ADRESS, TAG_BEER, TAG_SHOT }, 
+        		   				new int[] {	R.id.name,R.id.gender, R.id.adress, R.id.beer, R.id.shot });
            list.setAdapter(adapter);
            
            list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                    @Override
-                   public void onItemClick(AdapterView<?> parent, View view,
-                                           int position, long id) {
+                   public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                        Toast.makeText(MainActivity.this, clubList.get(+position).get("name"), Toast.LENGTH_SHORT).show();
                    }
                });
@@ -126,9 +138,5 @@ public class MainActivity extends Activity {
          e.printStackTrace();
        }
       }
-   }
-	
-	
-
-      
+   }   
 }
